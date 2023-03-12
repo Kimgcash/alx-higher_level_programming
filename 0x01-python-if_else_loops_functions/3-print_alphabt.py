dd8a94792660f75f64ae4dc8ce7276ac209ce878
Kimgcash <olugbamidayodavid88@gmail.com>
2023-03-12 11:17:25 -0700

    print without q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Olugbami Dayo David
+for letter in range(97, 123):
+    if char(letter) != 'q' and char(letter) != 'e':
+        print("{}.format(char(letter)), end="")
