f11bc5cca94923090c70235503a749763e98c708
Kimgcash <olugbamidayodavid88@gmail.com>
2023-04-23 05:13:01 -0700

    more data structure

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     count = 0
-    for key,value in dict.items():
+    for key,value in a_dictionary.items():
         count += 1
-    retuen count
+    return count
