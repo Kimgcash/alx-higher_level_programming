815dec06d299777d7665a32df1f0cc4d1d8eefe8
Kimgcash <olugbamidayodavid88@gmail.com>
2023-04-26 05:01:14 -0700

    integer

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    tmp a_dictionary = a_dictionary.copy()
+    for x in tmp_dictionary.keys():
+        tmp_dictionary[2] *= 2
+    return (tmp_dictionary)
