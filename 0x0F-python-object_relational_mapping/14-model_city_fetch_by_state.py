825d0d6164c840c6ad8bce168f3d2f070e2e6d93
Kimgcash <olugbamidayodavid88@gmail.com>
2023-06-20 07:29:01 -0700

    update1

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,6 +13,6 @@ if __name__ "__main__"
     Base.matedate.create_all(engine)
     session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State.name, City.id, City.name)
+    for instance in (session.query(State.name, City.id, City.name)
 		    .filter(State.id == City.state_id)):
 	print(instance[0] + ": (" + str(instance[1] + ":) " + instance[2])
