c7c29a610273a2a6669ec7838d907f612c215f68
Kimgcash <olugbamidayodavid88@gmail.com>
2023-06-20 07:26:49 -0700

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/urs/bin/python3
+import sys 
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker 
+
+
+if __name__ == "__main__"
+    engine = create_engin('mysql+mysqldb://{}:{}@localhost:3306/{}'
+			  .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.matedate.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = Session()
+    new_instance = session.query(State).filter_by(id=2).first()
+    new_instance.name = 'New Mexico'
+    session.commit()
