c8f62c8514874b185c9597547a7c0df4bf051245
Kimgcash <olugbamidayodavid88@gmail.com>
2023-07-05 01:38:59 -0700

    Network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" A script that fetches 
--https://alx-intranet.hbtn.io/status
--use the package urllib
-""" 
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-if __name__ == "__main__":
-   import urllib.request
-   with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
